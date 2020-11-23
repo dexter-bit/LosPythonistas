13e43516b3723cde2a8d0c722a85ff315487b08c
jarjar721 <jarp820@gmail.com>
2020-11-23 17:19:02 -0400

    Moved main functions from main.py to Menu.py

---
+++ Menu.py
@@ -0,0 +1,140 @@
+import colorama
+
+#MENU
+def print_header():
+	print("""
+		Universidad Católica Andrés Bello
+		Ingeniería Informática
+		Programación en Python - Proyecto #1
+
+		Integrantes del equipo:
+		Luis Pinto - CI: 25210435
+		Dexter Ramos - CI: 24204991
+		José Andrés Rodríguez Pérez - CI: 27.663.836
+		Ángel Sucasas - CI: 28.027.948
+		
+		*****************************
+		*       PIZZERIA UCAB       *
+		*****************************
+		""")
+
+def seleccionar_tamaño(builder):
+    userResponse = ''
+
+    print("""
+        SELECCIONE EL TAMAÑO
+        Opciones: Grande ( g ) Mediana ( m ) Personal ( p )""", end = ": ")
+    userResponse = input()
+    correctSize = chooseSize(userResponse, builder)
+    if correctSize != 0:
+        pass
+    else:
+        print(colorama.Fore.RED + """
+            Debe seleccionar el tamaño correcto!!""")
+        print(colorama.Style.RESET_ALL)
+        seleccionar_tamaño(builder)
+
+def chooseSize(size, builder):
+    if size == "g":
+        builder.setSize("Grande")
+        builder.setPrice(580)		
+        return 1
+    elif size == "m":
+        builder.setSize("Mediana")		
+        builder.setPrice(430)	
+        return 1
+    elif size == "p":
+        builder.setSize("Personal")	
+        builder.setPrice(280)		
+        return 1		
+    else:
+        print('tamaño invalido')		
+    return 0
+
+def showIngredientsMenu():
+	print("""
+		SELECCIONE LOS INGREDIENTES
+		Ingredientes:
+		Jamón         (ja)
+		Champiñones   (ch)
+		Pimentón      (pi)
+		Doble Queso   (dq)
+		Aceitunas     (ac)
+		Pepperoni     (pp)
+		Salchichón    (sa)""")
+
+def selectIngredients(builder):
+	userResponse = ''
+
+	print("""
+		Indique el ingrediente (Pulse enter para terminar)""", end = ": ")
+	userResponse = input()	
+	correctIngredients = chooseIngredient(userResponse,builder)
+	if(userResponse=="" and not len(builder.getPizza())):
+		print("\n\t\tUsted ha seleccionado un pizza "+builder.getSize()+" Margarita")
+		return 0
+	elif (userResponse==""):
+		print("\n\t\tUsted a seleccionado una pizza "+builder.getSize()+" con: ",end= '')
+		builder.getIngre()
+		return 0
+	elif(correctIngredients==0):		
+		#print(colorama.Fore.RED +""" 
+		print("Debe seleccionar un ingrediente correcto!!")
+		#print(colorama.Style.RESET_ALL)
+		selectIngredients(builder)
+	else:
+		selectIngredients(builder)
+
+#MENU
+def chooseIngredient(choosenIngredients,builder):
+	if choosenIngredients == "ja":
+		builder.addHam()		
+	elif choosenIngredients == "ch":
+		builder.addMushrooms()
+	elif choosenIngredients == "pi":
+		builder.addPeppers()
+	elif choosenIngredients == "dq":
+		builder.addDoubleCheese()
+	elif choosenIngredients == "ac":
+		builder.addOlive()
+	elif choosenIngredients == "pp":
+		builder.addPepperoni()
+	elif choosenIngredients == "sa":
+		builder.addSalami()	
+	elif choosenIngredients == "":
+		return 1
+	else:
+		print('ingrediente invalido')		
+		return 0
+	return 1
+
+#menu
+def selectAnotherPizza():
+	print("""
+		¿Desea agregar otra pizza?[s/n]""", end = ": ")
+	userResponse = input()
+	if (userResponse == "s"):
+		return False
+	elif (userResponse == "S"):
+		return False 
+	elif (userResponse == "n"):
+		return True
+	elif (userResponse == "N"):
+		return True  
+	else:
+		print('Respuesta invalida, intente otra vez')
+		selectAnotherPizza()
+
+def showAllTargetPizzaData(builder):
+	#print("Usted selecciono una pizza "+ str(builder.getSize())+" con ")
+	#print("Subtotal a pagar por una pizza "+builder.getSize()+" : " + builder.getPrice())
+	print("""
+		*******************************************
+		""")
+
+def getTotalPizzasPrice(pizzas):
+	totalPrice = 0
+	for pizza in pizzas:
+		print(pizza)
+		totalPrice += pizza.getPrice()
+	return totalPrice
--- main.py
+++ main.py
@@ -2,150 +2,15 @@ from Kitchen import Kitchen
 from ParticularPizzaBuilder import ParticularPizzaBuilder
 from Pizza import Pizza
 from PizzaBuilder import PizzaBuilder
+import Menu
 
 from abc import ABC, abstractmethod, abstractproperty
 import colorama
 
-def selectAnotherPizza():
-	print("""
-		¿Desea agregar otra pizza?[s/n]""", end = ": ")
-	userResponse = input()
-	if (userResponse == "s"):
-		return False
-	elif (userResponse == "S"):
-		return False 
-	elif (userResponse == "n"):
-		return True
-	elif (userResponse == "N"):
-		return True  
-	else:
-		print('Respuesta invalida, intente otra vez')
-		selectAnotherPizza()
-	
-def chooseSize(size,builder):
-	if size == "g":
-		builder.setSize("Grande")
-		builder.setPrice(580)		
-		return 1
-	elif size == "m":
-		builder.setSize("Mediana")		
-		builder.setPrice(430)	
-		return 1
-	elif size == "p":
-		builder.setSize("Personal")	
-		builder.setPrice(280)		
-		return 1		
-	else:
-		print('tamaño invalido')		
-	return 0
-
-def chooseIngredient(choosenIngredients,builder):
-	if choosenIngredients == "ja":
-		builder.addHam()		
-	elif choosenIngredients == "ch":
-		builder.addMushrooms()
-	elif choosenIngredients == "pi":
-		builder.addPeppers()
-	elif choosenIngredients == "dq":
-		builder.addDoubleCheese()
-	elif choosenIngredients == "ac":
-		builder.addOlive()
-	elif choosenIngredients == "pp":
-		builder.addPepperoni()
-	elif choosenIngredients == "sa":
-		builder.addSalami()	
-	elif choosenIngredients == "":
-		return 1
-	else:
-		print('ingrediente invalido')		
-		return 0
-	return 1
-
-def print_header():
-	print("""
-		Universidad Católica Andrés Bello
-		Ingeniería Informática
-		Programación en Python - Proyecto #1
-
-		Integrantes del equipo:
-		Luis Pinto - CI: 25210435
-		Dexter Ramos - CI: 24204991
-		José Andrés Rodríguez Pérez - CI: 27.663.836
-		Ángel Sucasas - CI: 28.027.948
-		
-		*****************************
-		*       PIZZERIA UCAB       *
-		*****************************
-		""")
-
-def seleccionar_tamaño(builder):
-	userResponse = ''
-
-	print("""
-		SELECCIONE EL TAMAÑO
-		Opciones: Grande ( g ) Mediana ( m ) Personal ( p )""", end = ": ")
-	userResponse = input()
-	correctSize = chooseSize(userResponse,builder)
-	if correctSize != 0:
-		pass
-	else:
-		print(colorama.Fore.RED + """
-			Debe seleccionar el tamaño correcto!!""")
-		print(colorama.Style.RESET_ALL)
-		seleccionar_tamaño(builder)
-
-def showIngredientsMenu():
-	print("""
-		SELECCIONE LOS INGREDIENTES
-		Ingredientes:
-		Jamón         (ja)
-		Champiñones   (ch)
-		Pimentón      (pi)
-		Doble Queso   (dq)
-		Aceitunas     (ac)
-		Pepperoni     (pp)
-		Salchichón    (sa)""")
-
-def selectIngredients(builder):
-	userResponse = ''
-
-	print("""
-		Indique el ingrediente (Pulse enter para terminar)""", end = ": ")
-	userResponse = input()	
-	correctIngredients = chooseIngredient(userResponse,builder)
-	if(userResponse=="" and not len(builder.getPizza())):
-		print("\n\t\tUsted ha seleccionado un pizza "+builder.getSize()+" Margarita")
-		return 0
-	elif (userResponse==""):
-		print("\n\t\tUsted a seleccionado una pizza "+builder.getSize()+" con: ",end= '')
-		builder.getIngre()
-		return 0
-	elif(correctIngredients==0):		
-		#print(colorama.Fore.RED +""" 
-		print("Debe seleccionar un ingrediente correcto!!")
-		#print(colorama.Style.RESET_ALL)
-		selectIngredients(builder)
-	else:
-		selectIngredients(builder)
-
-def showAllTargetPizzaData(builder):
-	#print("Usted selecciono una pizza "+ str(builder.getSize())+" con ")
-	#print("Subtotal a pagar por una pizza "+builder.getSize()+" : " + builder.getPrice())
-	print("""
-		*******************************************
-		""")
-
-def getTotalPizzasPrice(pizzas):
-	totalPrice = 0
-	for pizza in pizzas:
-		print(pizza)
-		totalPrice += pizza.getPrice()
-	return totalPrice
-
 
 class main():	
 	exit = False
-	print_header()
+	Menu.print_header()
 	clientPizza = []
 
 	while exit == False:
@@ -157,15 +22,15 @@ class main():
 		
 		print("""
 			Pizza número """+ str(len(clientPizza)))
-		seleccionar_tamaño(builder)
-		showIngredientsMenu()
-		selectIngredients(builder)
-		exit = selectAnotherPizza()
-		showAllTargetPizzaData(builder)
+		Menu.seleccionar_tamaño(builder)
+		Menu.showIngredientsMenu()
+		Menu.selectIngredients(builder)
+		exit = Menu.selectAnotherPizza()
+		Menu.showAllTargetPizzaData(builder)
 
 	print("""
-		El pedido tiene un total de """ + str(len(clientPizza)) + """ pizza(s) por un monto de """ + str(getTotalPizzasPrice(clientPizza)))
+		El pedido tiene un total de """ + str(len(clientPizza)) + """ pizza(s) por un monto de """ + str(Menu.getTotalPizzasPrice(clientPizza)))
 	print("""
-		Gracias por su compra, regrese pronto
+		¡Gracias por su compra! Regrese pronto
 		
 		""")
